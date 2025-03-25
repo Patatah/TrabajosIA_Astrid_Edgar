a9df744e4453c78e5ba90afa555d342d23690948
Astrid Echeagaray <astrid.monserrath1404@gmail.com>
2025-03-24 20:24:25 -0700

    Leer datos

--- MODULO 2/Tarea3_EmailSpam/emailsv2.py
+++ MODULO 2/Tarea3_EmailSpam/emailsv2.py
@@ -1,4 +1,5 @@
 import re # expresiones regulares
+import pandas as pd # para leer el csv
 
 def limpieza_texto(texto):
     texto = texto.lower() # Minúsculas
@@ -8,3 +9,9 @@ def limpieza_texto(texto):
     # r (raw). Entre corchetes escribimos un conjunto. ^ es negación. \w alfanumerico. \s son espacios.
     texto = re.sub(r'[^\w\s]', '', texto)
     return texto
+
+# Con pandas podemos leer los datos y luego los guardamos en variables
+# spam_ham_dataset.csv
+data = pd.read_csv('MODULO 2/Tarea3_EmailSpam/spam_ham_dataset.csv')
+textos = data['text'].apply(limpieza_texto)  # Asi se llama la columna en el csv
+labels = data['label']  # "Ham" o "Spam" en el csv. 
