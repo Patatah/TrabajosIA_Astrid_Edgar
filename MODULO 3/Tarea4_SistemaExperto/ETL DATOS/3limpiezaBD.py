69931e8317b5f0588bc74bc52423bef3b4c058b0
Edgar Alán Fernández Covarrubias <edgar1mas2@gmail.com>
2025-05-07 13:53:19 -0700

    ultimas limpiezas

--- MODULO 3/Tarea4_SistemaExperto/ETL DATOS/3limpiezaBD.py
+++ MODULO 3/Tarea4_SistemaExperto/ETL DATOS/3limpiezaBD.py
@@ -8,9 +8,17 @@ def limpiarIngredientes(lista):
     Convierte ingredientes especificos a generales, usando muchas reglas.
     Retorna un string con la query a ejecutar para actualizar la base de datos.
     """
+    if lista == None:
+        return None
+        
     listaOriginal = lista.copy()
-    
+
+    removidos = 0
     for i in range(len(lista)):
+
+        if(i+removidos) >= len(lista): #Manejar cambios de length cuando removemos ingredientes
+            break
+
         lista[i]=re.sub(r'\s+', ' ', lista[i])
         ingrediente = lista[i]
         if "sugar" in ingrediente:
@@ -105,10 +113,131 @@ def limpiarIngredientes(lista):
         elif "vegetable oil" in ingrediente:
             lista[i] = "vegetable oil"
             continue
-
-
-
-
+        elif "worcestershire sauce" in ingrediente:
+            lista[i] = "worcestershire sauce"
+            continue
+        elif "black pepper" in ingrediente:
+            lista[i] = "black pepper"
+            continue
+        elif "sour cream" in ingrediente:
+            if "potato chips" in ingrediente:
+                continue
+            lista[i] = "sour cream"
+            continue
+        elif "simply" in ingrediente:
+            lista[i] = lista[i].replace("simply", "").strip()
+            continue
+        elif "extra virgin olive oil" in ingrediente:
+            lista[i] = "olive oil"
+            continue
+        elif "dijon mustard" in ingrediente:
+            lista[i] = "dijon mustard"
+            continue
+        elif "cheddar cheese" in ingrediente:
+            lista[i] = "cheddar cheese"
+            continue
+        elif "campbell's" in ingrediente:
+            lista[i] = lista[i].replace("campbell's", "").strip()
+            continue
+        elif "cream cheese" in ingrediente:
+            lista[i] = "cream cheese"
+            continue
+        elif "cayenne pepper" in ingrediente:
+            lista[i] = "cayenne pepper"
+            continue
+        elif "ground cumin" in ingrediente:
+            lista[i] = "cumin"
+            continue
+        elif "ground beef" in ingrediente:
+            lista[i] = "ground beef"
+            continue
+        elif "ground turkey" in ingrediente:
+            lista[i] = "ground turkey"
+            continue
+        elif "ground chicken" in ingrediente:
+            lista[i] = "ground chicken"
+        elif "mozzarella cheese" in ingrediente:
+            lista[i] = "mozzarella cheese"
+            continue
+        elif "parmesan cheese" in ingrediente:
+            lista[i] = "parmesan cheese"
+            continue
+        elif "heavy cream" in ingrediente:
+            lista[i] = "heavy cream"
+            continue
+        elif "chili powder" in ingrediente:
+            lista[i] = "chili powder"
+            continue
+        elif "dried oregano" in ingrediente:
+            lista[i] = "oregano"
+            continue
+        elif "dried basil" in ingrediente:
+            lista[i] = "basil"
+            continue
+        elif "dried thyme" in ingrediente:
+            lista[i] = "thyme"
+            continue
+        elif "dried parsley" in ingrediente:
+            lista[i] = "parsley"
+            continue
+        elif "dried rosemary" in ingrediente:
+            lista[i] = "rosemary"
+            continue
+        elif "dried dill" in ingrediente:
+            lista[i] = "dill"
+            continue
+        elif "juice of" == ingrediente:
+            lista.remove(ingrediente)
+            removidos +=1
+            continue
+        elif "chicken breast" in ingrediente:
+            lista[i] = "chicken breast"
+            continue
+        elif "chicken thighs" in ingrediente:
+            lista[i] = "chicken thighs"
+            continue
+        elif "chicken legs" in ingrediente:
+            lista[i] = "chicken legs"
+            continue
+        elif "chicken wings" in ingrediente:
+            lista[i] = "chicken wings"
+            continue
+        elif "ketchup" in ingrediente:
+            lista[i] = "ketchup"
+            continue
+        elif "balsamic vinegar" in ingrediente:
+            lista[i] = "balsamic vinegar"
+            continue
+        elif "ground ginger" in ingrediente:
+            lista[i] = "ginger"
+            continue
+        elif "diced tomatoes" in ingrediente:
+            lista[i] = "diced tomatoes"
+            continue
+        elif "tomato sauce" in ingrediente:
+            lista[i] = "tomato sauce"
+            continue
+        elif "tomato paste" in ingrediente:
+            lista[i] = "tomato paste"
+            continue
+        elif "chili sauce" in ingrediente:
+            lista[i] = "chili sauce"
+            continue
+        elif "soy sauce" in ingrediente:
+            lista[i] = "soy sauce"
+            continue
+        elif "rice vinegar" in ingrediente:
+            lista[i] = "rice vinegar"
+            continue
+        elif "white vinegar" in ingrediente:
+            lista[i] = "white vinegar"
+            continue
+        elif "red wine vinegar" in ingrediente:
+            lista[i] = "red wine vinegar"
+            continue
+        elif "apple cider vinegar" in ingrediente:
+            lista[i] = "apple cider vinegar"
+            continue
 
 
 
