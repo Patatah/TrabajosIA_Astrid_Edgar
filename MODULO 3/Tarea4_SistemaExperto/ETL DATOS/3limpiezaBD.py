d1877af763255f8f4019ff7d26c006472e68c6ae
Edgar Alán Fernández Covarrubias <edgar1mas2@gmail.com>
2025-05-07 18:20:40 -0700

    Sin cotiniu

--- MODULO 3/Tarea4_SistemaExperto/ETL DATOS/3limpiezaBD.py
+++ MODULO 3/Tarea4_SistemaExperto/ETL DATOS/3limpiezaBD.py
@@ -23,98 +23,67 @@ def limpiarIngredientes(lista):
         ingrediente = lista[i]
         if "sugar" in ingrediente:
             if "free" in ingrediente:
-                string = ingrediente.replace("sugar free", "").strip()
-                string = ingrediente.replace("sugar-free", "").strip()
-                continue
+                lista[i] = ingrediente.replace("sugar free", "").strip()
+                lista[i] = ingrediente.replace("sugar-free", "").strip()
             if "brown" not in ingrediente:
                 lista[i] = 'sugar'
             else:
                 lista[i] = 'brown sugar'
-            continue
         elif "fat-free" in ingrediente:
             lista[i] = lista[i].replace("fat-free", "").strip()
-            continue
         elif "fat free" in ingrediente:
             lista[i] = lista[i].replace("fat free", "").strip()
-            continue
         elif "nonfat" in ingrediente:
             lista[i] = lista[i].replace("nonfat", "").strip()
-            continue
         elif "non fat" in ingrediente:
             lista[i] = lista[i].replace("non fat", "").strip()
-            continue
         elif "non-fat" in ingrediente:
             lista[i] = lista[i].replace("non-fat", "").strip()
-            continue
         elif "substitute" in ingrediente:
             lista[i] = lista[i].replace("substitute", "").strip()
-            continue
         elif "margerine" in ingrediente:
             lista[i] = "margarine"
-            continue
         elif "beef broth" in ingrediente:
             lista[i] = "beef broth"
-            continue
         elif "chicken broth" in ingrediente:
             lista[i] = "beef broth"
-            continue
         elif "eggs" in ingrediente:
-            if("chocolate" in ingrediente or "quail" in ingrediente or "powdered" in ingrediente or "pickled" in ingrediente):
-                continue
-            lista[i] = "eggs"
-            continue
+            if not("chocolate" in ingrediente or "quail" in ingrediente or "powdered" in ingrediente or "pickled" in ingrediente):
+                lista[i] = "eggs"
         elif "chocolate" in ingrediente and "egg" in ingrediente:
             lista[i] = "chocolate eggs"
-            continue
         elif ingrediente == "egg":
             lista[i] = "eggs"
-            continue
         elif "lemon juice" in ingrediente:
             lista[i] = "lemon juice"
-            continue
         elif "orange juice" in ingrediente:
             lista[i] = "orange juice"
-            continue
         elif "pineapple juice" in ingrediente:
-            if "teriyaki" in ingrediente:
-                continue
-            lista[i] = "pineapple juice"
-            continue
+            if "teriyaki" not in ingrediente:
+                lista[i] = "pineapple juice"
         elif "apple juice" in ingrediente:
-            if "pineapple" in ingrediente:
-                continue
-            lista[i] = "apple juice"
-            continue
+            if "pineapple" not in ingrediente:
+                lista[i] = "apple juice"
         elif "all-purpose flour" in ingrediente:
             lista[i] = "flour"
-            continue
         elif "kosher salt" in ingrediente:
             lista[i] = "salt"
-            continue
         elif "sea salt" in ingrediente:
             lista[i] = "salt"
-            continue
         elif "garlic clove" in ingrediente:
             lista[i] = "garlic"
-            continue
         elif "fresh garlic" in ingrediente:
             lista[i] = "garlic"
-            continue
         elif "fresh" in ingrediente:
             lista[i] = lista[i].replace("fresh", "").strip()
-            continue
         elif "baking powder" in ingrediente:
             lista[i] = "baking powder"
-            continue
         elif "vegetable oil" in ingrediente:
             lista[i] = "vegetable oil"
-            continue
         elif "worcestershire sauce" in ingrediente:
             lista[i] = "worcestershire sauce"
-            continue
         elif "black pepper" in ingrediente:
             lista[i] = "black pepper"
-            continue
         elif "sour cream" in ingrediente:
             if "potato chips" in ingrediente:
                 continue
